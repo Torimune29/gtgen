a6e5c83f1c2d65c35a00eb78acce1b410a34d09d
bfkwl2vu4eom <bfkwl2vu4eom@users.noreply.github.com>
2022-06-05 02:54:25 +0000

    deploy: 995d15b62116acc0d8d491cf6859ba57a722f388

--- class_function_parser_cpp_ast.js
+++ class_function_parser_cpp_ast.js
@@ -2,5 +2,5 @@ var class_function_parser_cpp_ast =
 [
     [ "FunctionParserCppAst", "class_function_parser_cpp_ast.html#a757fa088c9727506f0c3b92f57cd5011", null ],
     [ "~FunctionParserCppAst", "class_function_parser_cpp_ast.html#a95169c889377ba3db70f30634c95c15d", null ],
-    [ "GetFunctionNames", "class_function_parser_cpp_ast.html#ac10bd4c7079f114c6f31998d58e4c01f", null ]
+    [ "GetFunctionInfos", "class_function_parser_cpp_ast.html#a4e3e7dc4e08fb355cd91f55f487118b9", null ]
 ];
