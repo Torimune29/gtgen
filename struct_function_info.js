1d25979cdc7c322ecf6c94f8662d261f68137e2d
bfkwl2vu4eom <bfkwl2vu4eom@users.noreply.github.com>
2022-06-05 07:02:14 +0000

    deploy: dca7cbc79667a86b912e6b44ee949bff66cddd3d

--- struct_function_info.js
+++ struct_function_info.js
@@ -2,5 +2,6 @@ var struct_function_info =
 [
     [ "base", "struct_function_info.html#a425ff05a53639f5a5dbec93ae5a2dc3a", null ],
     [ "is_extern", "struct_function_info.html#a029827f36fe5291a90f5d6815cb9b476", null ],
-    [ "is_static", "struct_function_info.html#a89a47a986c4604fb3f60587b083601f0", null ]
+    [ "is_static", "struct_function_info.html#a89a47a986c4604fb3f60587b083601f0", null ],
+    [ "namespace_name", "struct_function_info.html#af565f4e3fc6fad79677a3a19287af909", null ]
 ];
