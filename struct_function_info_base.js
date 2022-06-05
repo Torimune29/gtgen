f9102f0f11fad4edea89cd7017e42834b2636961
bfkwl2vu4eom <bfkwl2vu4eom@users.noreply.github.com>
2022-06-05 08:12:14 +0000

    deploy: f69e1ac6d0f34c488029cc0502d9a6eb47a4fa04

--- struct_function_info_base.js
+++ struct_function_info_base.js
@@ -3,6 +3,7 @@ var struct_function_info_base =
     [ "is_consteval", "struct_function_info_base.html#a8f6178ad4111d7b74a694a20cdc9468c", null ],
     [ "is_constexpr", "struct_function_info_base.html#ae6290dd1a6a8fced103569a68ec55305", null ],
     [ "is_noexcept", "struct_function_info_base.html#ae43bc616e5b3e9352f62d4c68608495e", null ],
+    [ "is_variadic", "struct_function_info_base.html#adbff0c51a1e7e77e5621437be349efd5", null ],
     [ "name", "struct_function_info_base.html#a4f97312e655f23d09322c238feeed8dd", null ],
     [ "return_type", "struct_function_info_base.html#aade7c6eaf887dc6d22d0ad11ea58897e", null ],
     [ "signature", "struct_function_info_base.html#ad6e3fb462bdb5402b3d91641932c2d69", null ]
