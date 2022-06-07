580dc86d8ed6dc99b68d7c90c1e086398a7a66ce
Torimune29 <Torimune29@users.noreply.github.com>
2022-06-07 15:53:45 +0000

    deploy: f0ec5509a1b16cb2f21ce1367029cab9bd6193bf

--- class_function_parser.js
+++ class_function_parser.js
@@ -3,7 +3,7 @@ var class_function_parser =
     [ "FunctionParser", "class_function_parser.html#ad30ba1ebeefbe7d70b878363d60da644", null ],
     [ "FunctionParser", "class_function_parser.html#aa6cf12b5a051ab3f5e44267327d129c6", null ],
     [ "~FunctionParser", "class_function_parser.html#ae512ce7bf421cd20a279547f64efa9a4", null ],
-    [ "GetFunctionInfos", "class_function_parser.html#acfb1951792273b39f85f54170213cef0", null ],
-    [ "GetMemberFunctionInfos", "class_function_parser.html#a9340a7114b50a91d72a8fbd4b76d3d9e", null ],
+    [ "GetFunction", "class_function_parser.html#a8ef87851a22db9ed2c17dad5099e427c", null ],
+    [ "GetMemberFunction", "class_function_parser.html#a34b77e6b1d77979400149dcd4db47293", null ],
     [ "Ready", "class_function_parser.html#a71a08ef748de5215727c3bc6031f482f", null ]
 ];
--- class_function_parser_impl.js
+++ class_function_parser_impl.js
@@ -1,8 +1,7 @@
 var class_function_parser_impl =
 [
     [ "FunctionParserImpl", "class_function_parser_impl.html#a3d71ebcd696db2f83b29b38bb6603eae", null ],
-    [ "FunctionParserImpl", "class_function_parser_impl.html#aa915fe84270401140faf0b9c999ca2d0", null ],
     [ "~FunctionParserImpl", "class_function_parser_impl.html#a5861697e8f06f22156d8ca77e5144692", null ],
-    [ "GetFunctionInfos", "class_function_parser_impl.html#adc083e22e1b0c672b1c8c87fad671d05", null ],
-    [ "GetMemberFunctionInfos", "class_function_parser_impl.html#ac7b029bbedfa3a4e210c3f5e68065cd1", null ]
+    [ "GetFunction", "class_function_parser_impl.html#a35d42bac31ef6215528f86745f669af6", null ],
+    [ "GetMemberFunction", "class_function_parser_impl.html#a2df8f9ea999255554b5690e15d0ce51d", null ]
 ];
