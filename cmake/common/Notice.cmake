5150b82135577239828b0b7d3f9f7b73ecc578be
Torimune29 <71534350+Torimune29@users.noreply.github.com>
2022-06-10 10:21:41 +0900

    change LICENSE, fix NOTICE generator.

--- cmake/common/Notice.cmake
+++ cmake/common/Notice.cmake
@@ -5,7 +5,7 @@
 # 'make notice' searches LICENCE in dependent library repository and generate NOTICE.
 if(CMAKE_BUILD_TYPE STREQUAL "Release")
 add_custom_target(notice
-    rm -rf NOTICE && find ${CMAKE_CURRENT_BINARY_DIR}/_deps -name "LICENSE*" | xargs -I {} sh -c "echo '\\n--\\n' >> NOTICE && basename \$(dirname {}) | sed -e 's/\\-src//g' >> NOTICE && echo '\\n---\\n' >> NOTICE && cat {} >> NOTICE"
+    rm -rf NOTICE && echo "This project uses code from the following third-party projects, listed here\\nwith the full text of their respective licenses.\\n" >> NOTICE && find ${CMAKE_CURRENT_BINARY_DIR}/_deps -name "LICENSE*" | xargs -I {} sh -c "echo '\\n--\\n' >> NOTICE && basename \$(dirname {}) | sed -e 's/\\-src//g' >> NOTICE && echo '\\n---\\n' >> NOTICE && cat {} >> NOTICE"
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMENT "${BoldMagenta}Generating NOTICE documentation.${ColourReset}" VERBATIM
 )
