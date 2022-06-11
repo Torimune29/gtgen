93a8d3d16a9e9b465747ca155b94253dcf41b402
Torimune29 <Torimune29@users.noreply.github.com>
2022-06-11 10:42:38 +0000

    deploy: b56bf731984a3a2982b2fb9ddfb38c5ea43edc29

--- struct_function_info_base.js
+++ struct_function_info_base.js
@@ -1,5 +1,6 @@
 var struct_function_info_base =
 [
+    [ "InScopeOf", "struct_function_info_base.html#aba9ea6975a654017937380d0a039c7e2", null ],
     [ "operator==", "struct_function_info_base.html#a1328156eaf47d5de0c595598dc772e3c", null ],
     [ "is_consteval", "struct_function_info_base.html#a8f6178ad4111d7b74a694a20cdc9468c", null ],
     [ "is_constexpr", "struct_function_info_base.html#ae6290dd1a6a8fced103569a68ec55305", null ],
@@ -9,6 +10,6 @@ var struct_function_info_base =
     [ "name", "struct_function_info_base.html#a4f97312e655f23d09322c238feeed8dd", null ],
     [ "parameters", "struct_function_info_base.html#ac2ecd66cafab17d770f9e93345198dca", null ],
     [ "return_type", "struct_function_info_base.html#aade7c6eaf887dc6d22d0ad11ea58897e", null ],
-    [ "scopes", "struct_function_info_base.html#ab75e9ca3330d8b957261ba398d8ff37f", null ],
+    [ "scope", "struct_function_info_base.html#a140a23f77644891f38a44bdb56e42aac", null ],
     [ "signature", "struct_function_info_base.html#ad6e3fb462bdb5402b3d91641932c2d69", null ]
 ];
