5d652aecd0f2b276d2a37707086e077bd8b75902
Torimune29 <71534350+Torimune29@users.noreply.github.com>
2022-06-14 00:29:32 +0900

    fix error.

--- src/IncludeAnalyzerCppAst.cpp
+++ src/IncludeAnalyzerCppAst.cpp
@@ -20,22 +20,22 @@ std::vector<IncludeInfo> IncludeAnalyzerCppAst::GetIncludes() noexcept {
     cppast::visit(file, IncludeWhiteList(), [&](const cppast::cpp_entity &e, cppast::visitor_info info) {
       if (info.event == cppast::visitor_info::container_entity_exit) return true;
       if (e.kind() == cppast::cpp_include_directive::kind()) {
-        IncludeInfo info = {};
+        IncludeInfo info_include = {};
         const auto &include = reinterpret_cast<const cppast::cpp_include_directive &>(e);
 
         p_parser_->Log("Include directive in:", include.target().name(), cppast::severity::debug);
 
         // kind
-        info.kind = include.include_kind() == cppast::cpp_include_kind::system ? IncludeInfo::Kind::kSystem
-                                                                               : IncludeInfo::Kind::kLocal;
+        info_include.kind = include.include_kind() == cppast::cpp_include_kind::system ? IncludeInfo::Kind::kSystem
+                                                                                       : IncludeInfo::Kind::kLocal;
         // name
-        info.name = include.target().name();
+        info_include.name = include.target().name();
         // name
-        info.full_path = include.full_path();
+        info_include.full_path = include.full_path();
 
         p_parser_->Log("Include directive out:", include.target().name(), cppast::severity::debug);
 
-        infos.push_back(info);
+        infos.push_back(info_include);
       }
       return true;
     });
